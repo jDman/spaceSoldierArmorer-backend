57501f87898ae027fc134a19a83866e8a1d0d11f
jDman <davis.jaym@gmail.com>
2020-03-15 14:17:21 +0000

    feat - add testing libs mocha and chai and set up testing script

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
-const flash = require('connect-flash');
 const multer = require('multer');
 
 const MONGODB_URI = require('./database/connection-uri');
@@ -22,21 +20,13 @@ const csrfProtection = csrf();
 
 app.use(bodyParser.json());
 
-app.use(
-  session({
-    secret: SESSION_KEY,
-    resave: false,
-    saveUninitialized: false,
-    store: store
-  })
-);
-
 app.use(csrfProtection);
 
 app.use((req, res, next) => {
-  res.setHeader(('Access-Control-Allow-Origin', '*'));
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    ('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
---
+++ test/test.spec.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai');
+
+describe('testing test', () => {
+  it('true is true', () => {
+    expect(true).to.equal(true);
+  });
+});
